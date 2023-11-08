9727f335c4120c9950124cbb1d32210a6d2aa3a3
Daniel Ruiz Vargas <daniel@rendermotion.com>
2023-11-08 10:29:17 -0500

    Update arm_rig_IKFK_5.py
    Make the ik fk scriot work

--- Mathilde/arm_rig_IKFK_5.py
+++ Mathilde/arm_rig_IKFK_5.py
@@ -12,37 +12,40 @@ def arm_rig_fk(*locators):
     # Creates a parent_joint to keep track of the last joint created
     parent_joint = None
 
-    for each_locator in locators:
+    for index,  each_locator in enumerate(locators):
         new_joint = pm.joint()
         pm.matchTransform(new_joint, each_locator)
         pm.makeIdentity(new_joint, apply=True, translate=True, rotate=True, scale=True)
 
         # If the joint is a parent_joint, parent it with the latest joint created
-        if parent_joint:
-            pm.parent(new_joint, parent_joint)
+        # if parent_joint:
+        #     pm.parent(new_joint, parent_joint)
 
         parent_joint = new_joint
         joints_list_fk.append(new_joint)
-        pm.rename(new_joint, 'fk_joint')
+        pm.rename(new_joint, f'fk_joint{index}')
 
-    controls_fk = control_creation(*joints_list_fk)
+    reset_control_list, controls_fk = control_creation(*joints_list_fk)
+    for each_control, each_joint in zip(controls_fk, joints_list_fk):
+        pm.parentConstraint(each_control, each_joint)
+    # Parent the reset control to the previous control
+    for index, reset_control in enumerate(reset_control_list[1:]):
 
-    for control in controls_fk:
-        pm.parent(control, fk_arm_group)
+        pm.parent(reset_control, controls_fk[index])
 
 
 def arm_rig_ik(*locators):
     joints_list_ik = []
     pm.select(clear=True)
-    for each_locator in locators:
+    for index, each_locator in enumerate(locators):
         new_joint = pm.joint()
         pm.matchTransform(new_joint, each_locator)
         pm.makeIdentity(new_joint, apply=True, translate=True, rotate=True, scale=True)
         joints_list_ik.append(new_joint)
-        pm.rename(new_joint, 'ik_joint')
+        pm.rename(new_joint, f'ik_joint{index}')
 
         # Call the create_control functions
-    controls_ik = control_creation(locators[-1])
+    reset_controls, controls_ik = control_creation(locators[-1])
     # Create IK Handle
     ik_handle, effector = pm.ikHandle(startJoint=joints_list_ik[0], endEffector=joints_list_ik[-1], solver='ikSCsolver')
     pm.parentConstraint(controls_ik, ik_handle)
@@ -51,12 +54,16 @@ def arm_rig_ik(*locators):
 
 def control_creation(*joints):
     control_list = []
-    for each_joint in joints:
+    reset_control_list = []
+    for index, each_joint in enumerate(joints):
+        reset_group = pm.group(name=f'reset{index}', empty=True)
         control, make_nurb_circle = pm.circle(normal=[1, 0, 0])
         control_list.append(control)
         pm.matchTransform(control, each_joint)
-        pm.parentConstraint(control, each_joint)
-    return control_list
+        pm.matchTransform(reset_group, each_joint)
+        reset_control_list.append(reset_group)
+        pm.parent(control, reset_group)
+    return reset_control_list, control_list
 
 
 def create_control_on_transform(maya_node):
@@ -95,6 +102,7 @@ locator_list = pm.ls(selection=True)
 arm_rig_fk(*locator_list)
 arm_rig_ik(*locator_list)
 
-ik_joints_list = pm.ls('ik_joint*')
-fk_joints_list = pm.ls('fk_joint*')
+
+ik_joints_list = pm.ls('ik_joint*', type='joint')
+fk_joints_list = pm.ls('fk_joint*', type='joint')
 constraint_nodes(ik_joints_list, fk_joints_list)
